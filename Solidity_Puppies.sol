6eafbb8279341d16ec69c0f5698e1240a13748de
adriancarrillo-acm <42261260+adriancarrillo-acm@users.noreply.github.com>
2018-08-14 17:35:51 -0500

    internal contract attribute added

--- Solidity_Puppies.sol
+++ Solidity_Puppies.sol
@@ -11,7 +11,8 @@ contract PuppiesList {
 
     mapping (address => uint) ownerOfPuppies;
 
-    function addPuppy(string _name, uint _age) {
+    function addPuppy(string _name, uint _age) internal {  /*"internal" allows access to the function from within original contracts
+                                                             and contracts that inherit from it.*/
         address owner = msg.sender;
         uint id = pups.push(Dog(_name, _age));
         ownerOfPuppies[owner] = id;             //Maps the puppy ID to the address of the contract owner.
