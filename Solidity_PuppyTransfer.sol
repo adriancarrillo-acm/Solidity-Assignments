b8652fe652c04e3ebd9a11167366e70788c96d97
adriancarrillo-acm <42261260+adriancarrillo-acm@users.noreply.github.com>
2018-08-14 19:27:09 -0500

    Added error-checks: "require" and "assert"

--- Solidity_PuppyTransfer.sol
+++ Solidity_PuppyTransfer.sol
@@ -6,9 +6,11 @@ contract PupTransfer is PuppiesList {        //Inherit everything from PuppiesLi
 
     function transferDog(address _newOwner) returns (uint) {     //Transfer dog to the address of the new owner.
         address owner = msg.sender;
+        require (owner != _newOwner);      //Error handling to prevent owner from making a transaction to themselves.
         uint dogId = ownerOfPuppies[owner];
         delete(ownerOfPuppies[owner]);        //Delete previous ownership.
         ownerOfPuppies[_newOwner] = dogId;    //Assign new ownership.
+        assert(ownerOfPuppies[owner] == 0);   //Error check that asserts that the transfer was successful. 
     }
 
     function addKennelDog(string _name, uint _age) {
